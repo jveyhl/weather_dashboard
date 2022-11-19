ef9aaf88068bd957293844f28564c115d1c33581
jve <joe.veyhl@gmail.com>
2022-11-18 21:47:50 -0500

    able to display date, temperature, wind speed and humidity for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,10 @@ $(document).ready(() => {
     // Display Results Elements
     var currentWeatherEl = $("#current-weather");
     const cityNameEl = $("#city-name");
-    const currentImgEl = $("#current-img");
+    const currentIconEl = $("#current-icon");
     const currentTempEl = $("#temperature");
-    const currentHumEl = $("#humidity");
     const currentWindEl = $("#wind-speed");
+    const currentHumEl = $("#humidity");
     const currentUvEl = $("#uv-index");
     var fiveDayEl = $("#fiveday-header");
 
@@ -31,16 +31,44 @@ $(document).ready(() => {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
-                
-                // Display date/time next to city name in current conditions
-                const currentDate = dayjs().format("DD MMMM YYYY h:mm A"); 
 
-                console.log(currentDate);
+                // Make current conditions element visible
+                currentWeatherEl[0].classList.remove("d-none")
+
+                // Create date/time to place next to city name in current conditions
+                const currentDate = dayjs().format("DD MMMM YYYY");
+
+                // Display city name and current date im current conditions
+                cityNameEl[0].innerHTML = `${data.name} ${currentDate}`;
+
+                // Get the weather icon value
+                let weatherIcon = data.weather[0].icon;
+                // Set the <img> elements src attribute to get the icon image
+                currentIconEl[0].setAttribute("src", `https://openweathermap.org/img/wn/${weatherIcon}.png`);
+
+                // Set current temperture
+                currentTempEl[0].innerHTML = `Temperature: ${tempConvert(data.main.temp).f} &#176F`;
+                // Set current humidity
+                currentHumEl[0].innerHTML = `Humidity: ${data.main.humidity}%`;
+                // Set current wind speed
+                currentWindEl[0].innerHTML = `Wind Speed: ${data.wind.speed} MPH`;
+
+                console.log();
             })
             ;
 
-        console.log("from getWeather");
+        //console.log("from getWeather");
+    }
+
+    // Convert kelvin to celsius
+    function tempConvert(temp) {
+        // Create temperature object
+        const allTemps = {
+            k: temp,
+            c: (temp - 273.15).toFixed(1),
+            f: (1.8 * (temp - 273.15) + 32).toFixed(1)
+        }
+        return allTemps;
     }
 
     getWeather("montreal");
